3d27dda72629947f62b43b0b157f7a369662f76c
thomashamer3 <60607850+thomashamer3@users.noreply.github.com>
2023-06-13 19:30:02 -0300

    Segundo Parcial Generala Cambios

---
+++ Hamer.Thomas.SegundoParcial/FormLogin.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Hamer.Thomas.SegundoParcial
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.lblGenerala = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btn_Aceptar = new System.Windows.Forms.Button();
+            this.btn_Registrarse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGenerala
+            // 
+            this.lblGenerala.AutoSize = true;
+            this.lblGenerala.BackColor = System.Drawing.Color.Transparent;
+            this.lblGenerala.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenerala.Location = new System.Drawing.Point(228, 22);
+            this.lblGenerala.Name = "lblGenerala";
+            this.lblGenerala.Size = new System.Drawing.Size(124, 26);
+            this.lblGenerala.TabIndex = 0;
+            this.lblGenerala.Text = "La Generala";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.BackColor = System.Drawing.Color.Transparent;
+            this.lblCorreo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.Location = new System.Drawing.Point(166, 69);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(74, 25);
+            this.lblCorreo.TabIndex = 1;
+            this.lblCorreo.Text = "Correo";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(171, 97);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(248, 20);
+            this.txtCorreo.TabIndex = 1;
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.BackColor = System.Drawing.Color.Transparent;
+            this.lblClave.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.Location = new System.Drawing.Point(166, 144);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(59, 25);
+            this.lblClave.TabIndex = 4;
+            this.lblClave.Text = "Clave";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(171, 172);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '*';
+            this.txtClave.Size = new System.Drawing.Size(248, 20);
+            this.txtClave.TabIndex = 2;
+            this.txtClave.UseSystemPasswordChar = true;
+            // 
+            // btn_Aceptar
+            // 
+            this.btn_Aceptar.AutoSize = true;
+            this.btn_Aceptar.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Aceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Aceptar.Location = new System.Drawing.Point(171, 216);
+            this.btn_Aceptar.Name = "btn_Aceptar";
+            this.btn_Aceptar.Size = new System.Drawing.Size(248, 30);
+            this.btn_Aceptar.TabIndex = 3;
+            this.btn_Aceptar.Text = "INGRESAR";
+            this.btn_Aceptar.UseVisualStyleBackColor = false;
+            this.btn_Aceptar.Click += new System.EventHandler(this.btn_Aceptar_Click);
+            // 
+            // btn_Registrarse
+            // 
+            this.btn_Registrarse.AutoSize = true;
+            this.btn_Registrarse.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Registrarse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Registrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Registrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Registrarse.Location = new System.Drawing.Point(171, 267);
+            this.btn_Registrarse.Name = "btn_Registrarse";
+            this.btn_Registrarse.Size = new System.Drawing.Size(248, 30);
+            this.btn_Registrarse.TabIndex = 4;
+            this.btn_Registrarse.Text = "REGISTRARSE";
+            this.btn_Registrarse.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(59)))), ((int)(((byte)(108)))));
+            this.ClientSize = new System.Drawing.Size(546, 418);
+            this.Controls.Add(this.btn_Registrarse);
+            this.Controls.Add(this.btn_Aceptar);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblGenerala);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGenerala;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btn_Aceptar;
+        private System.Windows.Forms.Button btn_Registrarse;
+    }
+}
+
