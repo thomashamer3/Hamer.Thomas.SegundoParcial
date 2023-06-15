7d65a91d9a72e2f8d785dd882839bdbf98dc19f1
thomashamer3 <60607850+thomashamer3@users.noreply.github.com>
2023-06-15 16:06:13 -0300

    Arreglo

---
+++ Estadistica.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Hamer.Thomas.SegundoParcial
+{
+    partial class Estadistica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Estadistica));
+            this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.lblPartidasJugadas = new System.Windows.Forms.Label();
+            this.lblCantidadDePartidasJugadas = new System.Windows.Forms.Label();
+            this.lblPartidasGanadas = new System.Windows.Forms.Label();
+            this.lblCantidadDePartidasGanadas = new System.Windows.Forms.Label();
+            this.lblPorcentajeDeVictoria = new System.Windows.Forms.Label();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombreUsuario
+            // 
+            this.lblNombreUsuario.AutoSize = true;
+            this.lblNombreUsuario.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreUsuario.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblNombreUsuario.Location = new System.Drawing.Point(353, 36);
+            this.lblNombreUsuario.Name = "lblNombreUsuario";
+            this.lblNombreUsuario.Size = new System.Drawing.Size(136, 22);
+            this.lblNombreUsuario.TabIndex = 2;
+            this.lblNombreUsuario.Text = "nombreUsuario";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.Location = new System.Drawing.Point(332, 417);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(179, 30);
+            this.btnVolver.TabIndex = 9;
+            this.btnVolver.Text = "VOLVER";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // lblPartidasJugadas
+            // 
+            this.lblPartidasJugadas.AutoSize = true;
+            this.lblPartidasJugadas.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartidasJugadas.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPartidasJugadas.Location = new System.Drawing.Point(12, 159);
+            this.lblPartidasJugadas.Name = "lblPartidasJugadas";
+            this.lblPartidasJugadas.Size = new System.Drawing.Size(153, 22);
+            this.lblPartidasJugadas.TabIndex = 10;
+            this.lblPartidasJugadas.Text = "Partidas Jugadas:";
+            // 
+            // lblCantidadDePartidasJugadas
+            // 
+            this.lblCantidadDePartidasJugadas.AutoSize = true;
+            this.lblCantidadDePartidasJugadas.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidadDePartidasJugadas.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCantidadDePartidasJugadas.Location = new System.Drawing.Point(171, 159);
+            this.lblCantidadDePartidasJugadas.Name = "lblCantidadDePartidasJugadas";
+            this.lblCantidadDePartidasJugadas.Size = new System.Drawing.Size(20, 22);
+            this.lblCantidadDePartidasJugadas.TabIndex = 11;
+            this.lblCantidadDePartidasJugadas.Text = "0";
+            // 
+            // lblPartidasGanadas
+            // 
+            this.lblPartidasGanadas.AutoSize = true;
+            this.lblPartidasGanadas.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartidasGanadas.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPartidasGanadas.Location = new System.Drawing.Point(334, 159);
+            this.lblPartidasGanadas.Name = "lblPartidasGanadas";
+            this.lblPartidasGanadas.Size = new System.Drawing.Size(155, 22);
+            this.lblPartidasGanadas.TabIndex = 12;
+            this.lblPartidasGanadas.Text = "Partidas Ganadas:";
+            // 
+            // lblCantidadDePartidasGanadas
+            // 
+            this.lblCantidadDePartidasGanadas.AutoSize = true;
+            this.lblCantidadDePartidasGanadas.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidadDePartidasGanadas.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCantidadDePartidasGanadas.Location = new System.Drawing.Point(495, 159);
+            this.lblCantidadDePartidasGanadas.Name = "lblCantidadDePartidasGanadas";
+            this.lblCantidadDePartidasGanadas.Size = new System.Drawing.Size(20, 22);
+            this.lblCantidadDePartidasGanadas.TabIndex = 13;
+            this.lblCantidadDePartidasGanadas.Text = "0";
+            // 
+            // lblPorcentajeDeVictoria
+            // 
+            this.lblPorcentajeDeVictoria.AutoSize = true;
+            this.lblPorcentajeDeVictoria.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentajeDeVictoria.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPorcentajeDeVictoria.Location = new System.Drawing.Point(591, 159);
+            this.lblPorcentajeDeVictoria.Name = "lblPorcentajeDeVictoria";
+            this.lblPorcentajeDeVictoria.Size = new System.Drawing.Size(194, 22);
+            this.lblPorcentajeDeVictoria.TabIndex = 14;
+            this.lblPorcentajeDeVictoria.Text = "Porcentaje de Victoria:";
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentaje.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPorcentaje.Location = new System.Drawing.Point(804, 159);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(20, 22);
+            this.lblPorcentaje.TabIndex = 15;
+            this.lblPorcentaje.Text = "0";
+            // 
+            // Estadistica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(59)))), ((int)(((byte)(108)))));
+            this.ClientSize = new System.Drawing.Size(841, 509);
+            this.Controls.Add(this.lblPorcentaje);
+            this.Controls.Add(this.lblPorcentajeDeVictoria);
+            this.Controls.Add(this.lblCantidadDePartidasGanadas);
+            this.Controls.Add(this.lblPartidasGanadas);
+            this.Controls.Add(this.lblCantidadDePartidasJugadas);
+            this.Controls.Add(this.lblPartidasJugadas);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.lblNombreUsuario);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Estadistica";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estadistica";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Estadistica_FormClosing);
+            this.Load += new System.EventHandler(this.Estadistica_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombreUsuario;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label lblPartidasJugadas;
+        private System.Windows.Forms.Label lblCantidadDePartidasJugadas;
+        private System.Windows.Forms.Label lblPartidasGanadas;
+        private System.Windows.Forms.Label lblCantidadDePartidasGanadas;
+        private System.Windows.Forms.Label lblPorcentajeDeVictoria;
+        private System.Windows.Forms.Label lblPorcentaje;
+    }
+}
---
+++ Estadistica.cs
@@ -0,0 +1,97 @@
+﻿using Entidades;
+using System;
+using System.Windows.Forms;
+
+namespace Hamer.Thomas.SegundoParcial
+{
+    public partial class Estadistica : Form
+    {
+        private BaseDeDatos ado = new BaseDeDatos();
+        private Estadisticas estadistica = new Estadisticas();
+        private int id;
+        private double partidasGanadas;
+        private double partidasJugadas;
+        private double porcentaje;
+        private Usuarios usuario = new Usuarios();
+        private string usuarioIngresado;
+
+        public Estadistica()
+        {
+            InitializeComponent();
+        }
+
+        public int Id { get => id; set => id = value; }
+        public double PartidasGanadas { get => partidasGanadas; set => partidasGanadas = value; }
+        public double PartidasJugadas { get => partidasJugadas; set => partidasJugadas = value; }
+        public double Porcentaje { get => porcentaje; set => porcentaje = value; }
+        public string UsuarioIngresado { get => usuarioIngresado; set => usuarioIngresado = value; }
+
+        /// <summary>
+        /// La función cierra el formulario actual cuando se hace clic en el botón "Volver".
+        /// </summary>
+        /// <param name="sender">El objeto que generó el evento. En este caso, es el botón en el que se
+        /// hizo clic (btnVolver).</param>
+        /// <param name="EventArgs">EventArgs es una clase en C# que proporciona datos para un evento.
+        /// No contiene datos, pero se utiliza como clase base para las clases que proporcionan datos de
+        /// eventos. En este fragmento de código específico, EventArgs se usa como un parámetro para el
+        /// método del controlador de eventos btnVolver_Click, que se llama cuando</param>
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Esta función muestra al usuario un cuadro de mensaje que le pregunta si desea cerrar el
+        /// programa y cancela el evento de cierre si el usuario selecciona "No".
+        /// </summary>
+        /// <param name="sender">El objeto que generó el evento, en este caso, el formulario
+        /// Estadistica.</param>
+        /// <param name="FormClosingEventArgs">Es un argumento de evento que se pasa al controlador de
+        /// eventos FormClosing. Contiene información sobre el evento y permite al controlador cancelar
+        /// el cierre del formulario si es necesario.</param>
+        /// <returns>
+        /// Si el usuario selecciona "No" en el cuadro de mensaje, el controlador de eventos establece
+        /// la propiedad `e.Cancel` en `true`, lo que significa que el evento de cierre del formulario
+        /// se cancelará y el formulario permanecerá abierto. Si el usuario selecciona "Sí" o cierra el
+        /// cuadro de mensaje, el controlador de eventos no devuelve nada de forma explícita.
+        /// </returns>
+        private void Estadistica_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.Cancel)
+            {
+                if (MessageBox.Show("Deseas Cerrar La Ventana?", "Salir",
+                     MessageBoxButtons.YesNo, MessageBoxIcon.Question).Equals(DialogResult.Yes))
+                {
+                    Dispose();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Esta función carga y muestra estadísticas de un usuario, incluido su nombre de usuario, la
+        /// cantidad de juegos ganados y jugados y su porcentaje de ganancias.
+        /// </summary>
+        /// <param name="sender">El objeto que generó el evento.</param>
+        /// <param name="EventArgs">EventArgs es una clase en C# que proporciona datos para un evento.
+        /// Se utiliza como clase base para clases que representan datos de eventos. En este código
+        /// específico, el método Estadistica_Load es un controlador de eventos para el evento Load de
+        /// un formulario, y el parámetro EventArgs se pasa automáticamente por el</param>
+        private void Estadistica_Load(object sender, EventArgs e)
+        {
+            usuario.NombreUsuario = usuarioIngresado;
+            id = ado.ObtenerIDUsuario(usuario);
+            estadistica = ado.SelectById(id);
+            partidasGanadas = estadistica.PartidasGanadas;
+            partidasJugadas = estadistica.PartidasJugadas;
+            lblNombreUsuario.Text = UsuarioIngresado;
+            lblPartidasGanadas.Text = partidasGanadas.ToString();
+            lblPartidasJugadas.Text = partidasJugadas.ToString();
+            porcentaje = (partidasGanadas / partidasJugadas) * 100;
+            lblPorcentaje.Text = porcentaje.ToString() + "%";
+        }
+    }
+}
